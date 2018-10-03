a9c067067caa456048688df77a4b5edfbbc7f9b0
Fuzzlemann <zhang.paul@ymail.com>
2018-10-03 13:42:45 +0200

    Shortened InternetTestCommand

--- src/main/src/de/fuzzlemann/ucutils/commands/InternetTestCommand.java
+++ src/main/src/de/fuzzlemann/ucutils/commands/InternetTestCommand.java
@@ -2,8 +2,8 @@ package de.fuzzlemann.ucutils.commands;
 
 import de.fuzzlemann.ucutils.utils.command.Command;
 import de.fuzzlemann.ucutils.utils.command.CommandExecutor;
+import de.fuzzlemann.ucutils.utils.text.Message;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -21,22 +21,16 @@ import java.util.Objects;
  */
 @SideOnly(Side.CLIENT)
 public class InternetTestCommand implements CommandExecutor {
-    private final List<String> hosts = Arrays.asList("unicacity.de", "fuzzlemann.de", "www.google.de");
+    private final List<String> hosts = Arrays.asList("unicacity.de", "fuzzlemann.de", "google.de");
 
     @Override
     @Command(labels = {"internettest", "inettest"})
     public boolean onCommand(EntityPlayerSP p, String[] args) {
         new Thread(() -> {
-            TextComponentString textBegin = new TextComponentString("Ping zu ");
-            textBegin.getStyle().setColor(TextFormatting.AQUA);
+            Message.MessageBuilder builder = Message.builder();
 
-            TextComponentString textMid = new TextComponentString(": ");
-            textMid.getStyle().setColor(TextFormatting.AQUA);
-
-            for (String host : hosts) {
-                TextComponentString hostComponent = new TextComponentString(host);
-                hostComponent.getStyle().setColor(TextFormatting.AQUA);
-
+            for (int i = 0; i < hosts.size(); i++) {
+                String host = hosts.get(i);
                 String result;
                 try {
                     result = ping(host);
@@ -45,11 +39,15 @@ public class InternetTestCommand implements CommandExecutor {
                     return;
                 }
 
-                TextComponentString pingComponent = new TextComponentString(result);
-                pingComponent.getStyle().setColor(result.equals("Nicht erreichbar") ? TextFormatting.RED : TextFormatting.GOLD);
+                TextFormatting color = result.equals("Nicht erreichbar") ? TextFormatting.RED : TextFormatting.GOLD;
 
-                p.sendMessage(textBegin.createCopy().appendSibling(hostComponent).appendSibling(textMid).appendSibling(pingComponent));
+                builder.of("Ping zu " + host + ": ").color(TextFormatting.AQUA).advance()
+                        .of(result).color(color).advance();
+
+                if (i != hosts.size() - 1) builder.newLine();
             }
+
+            p.sendMessage(builder.build().toTextComponent());
         }).start();
         return true;
     }
