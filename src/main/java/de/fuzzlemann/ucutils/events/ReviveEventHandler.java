533990f09f7f6907c029a6f3a43682f4e513c9ac
Fuzzlemann <zhang.paul@ymail.com>
2019-07-16 15:36:23 +0200

    Adds revive on click on corpse

---
+++ src/main/java/de/fuzzlemann/ucutils/events/ReviveEventHandler.java
@@ -0,0 +1,38 @@
+package de.fuzzlemann.ucutils.events;
+
+import de.fuzzlemann.ucutils.Main;
+import de.fuzzlemann.ucutils.utils.abstraction.AbstractionHandler;
+import de.fuzzlemann.ucutils.utils.faction.Faction;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemSkull;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
+
+/**
+ * @author Fuzzlemann
+ */
+@Mod.EventBusSubscriber
+public class ReviveEventHandler {
+
+    @SubscribeEvent
+    public void onRevive(PlayerInteractEvent.RightClickBlock e) {
+        if (Faction.getFactionOfPlayer() != Faction.UCMD) return;
+
+        BlockPos pos = e.getPos();
+
+        List<EntityItem> items = Main.MINECRAFT.world.getEntities(EntityItem.class, (ent) -> ent != null && ent.hasCustomName() && ent.getItem().getItem() instanceof ItemSkull);
+
+        for (EntityItem entityItem : items) {
+            double distance = entityItem.getDistanceSq(pos.getX(), pos.getY(), pos.getZ());
+
+            if (distance < 2) {
+                AbstractionHandler.getInstance().getPlayer().sendChatMessage("/revive");
+                return;
+            }
+        }
+    }
+}
