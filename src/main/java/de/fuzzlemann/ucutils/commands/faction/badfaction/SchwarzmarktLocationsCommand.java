ca1ca1ff2834654e5c06e079e553fcb7299a51fd
Paul Zhang <me@paul-zhang.de>
2021-01-17 16:02:48 +0100

    Reactivates /schwarzmarktlocations

--- src/main/java/de/fuzzlemann/ucutils/commands/faction/badfaction/SchwarzmarktLocationsCommand.java
+++ src/main/java/de/fuzzlemann/ucutils/commands/faction/badfaction/SchwarzmarktLocationsCommand.java
@@ -1,9 +1,12 @@
 package de.fuzzlemann.ucutils.commands.faction.badfaction;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import de.fuzzlemann.ucutils.base.command.Command;
-import de.fuzzlemann.ucutils.base.text.TextUtils;
+import de.fuzzlemann.ucutils.base.text.Message;
+import de.fuzzlemann.ucutils.utils.location.navigation.NavigationUtil;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -17,13 +20,22 @@ import java.util.Map;
 public class SchwarzmarktLocationsCommand {
 
     private static final List<Map.Entry<String, BlockPos>> BLACK_MARKET_LIST = Lists.newArrayList(
-
+            Maps.immutableEntry("Brücke (Hausaddonshop)", new BlockPos(80, 58, -118)),
+            Maps.immutableEntry("Flughafen (UCA)", new BlockPos(-78, 63, 637)),
+            Maps.immutableEntry("Hafen", new BlockPos(-405, 69, 29)),
+            Maps.immutableEntry("Containerhalle", new BlockPos(-93, 69, -438)),
+            Maps.immutableEntry("Strand (Mex)", new BlockPos(-524, 66, -236)),
+            Maps.immutableEntry("U-Bahn (Kerzakov-Gebiet)", new BlockPos(849, 52, 262)),
+            Maps.immutableEntry("Yachthafen", new BlockPos(285, 63, -635)),
+            Maps.immutableEntry("Uran Berg", new BlockPos(-437, 167, 800)),
+            Maps.immutableEntry("Mühle (Chinatown)", new BlockPos(1225, 68, 198)),
+            Maps.immutableEntry("Tanzfläche (Las-Unicas)", new BlockPos(1341, 64, 355)),
+            Maps.immutableEntry("Schwimmbad (Las-Unicas)", new BlockPos(1647, 52, 241))
     );
 
-    @Command({"schwarzmarktlocations", "schwarzmarktlocs"})
+    @Command({"schwarzmarktlocations", "schwarzmarktlocs", "smarktlocs"})
     public boolean onCommand() {
-        TextUtils.error("Temporär deaktiviert; viel Spaß beim Suchen! ;)"); //todo
-        /*Message.builder()
+        Message.builder()
                 .of("» ").color(TextFormatting.DARK_GRAY).advance()
                 .of("Positionen aller möglichen Schwarzmärkte\n").color(TextFormatting.DARK_AQUA).advance()
                 .joiner(BLACK_MARKET_LIST)
@@ -35,7 +47,7 @@ public class SchwarzmarktLocationsCommand {
                             .of(name + ": ").color(TextFormatting.GRAY).advance()
                             .messageParts(NavigationUtil.getRawNavigationMessage(blockPos).getMessageParts());
                 }).newLineJoiner().advance()
-                .send();*/
+                .send();
         return true;
     }
 }
