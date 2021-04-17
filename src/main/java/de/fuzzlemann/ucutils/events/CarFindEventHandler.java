d72c4fc2da8b8036debd2d4bb1a5c7c4fa61a2b9
Dimiikou <leon.aissa@icloud.com>
2021-04-18 00:22:29 +0200

    Added a time constraint

--- src/main/java/de/fuzzlemann/ucutils/events/CarFindEventHandler.java
+++ src/main/java/de/fuzzlemann/ucutils/events/CarFindEventHandler.java
@@ -17,6 +17,9 @@ import java.util.regex.Pattern;
 @Mod.EventBusSubscriber
 @SideOnly(Side.CLIENT)
 public class CarFindEventHandler {
+
+    private static long executedTime = -1;
+
     private static final Pattern CAR_POSITION_MESSAGE = Pattern.compile("^\\[Car] Das Fahrzeug befindet sich bei . X: (-?\\d+) \\| Y: (-?\\d+) \\| Z: (-?\\d+)$");
     private static final Pattern ROUTE_PATTERNS = Pattern.compile("^Du hast keine Route\\.$" +
             "|^Du hast deine Route gelöscht\\.$");
@@ -27,11 +30,12 @@ public class CarFindEventHandler {
         if (!UCUtilsConfig.autoNavigationForCarFind) return;
 
         Matcher routeMatcher = ROUTE_PATTERNS.matcher(e.getMessage().getUnformattedText());
-        if (routeMatcher.find())
+        if (routeMatcher.find() && System.currentTimeMillis() - executedTime < 500L)
             e.setCanceled(true);
-        
+
         Matcher carPositionMessageMatcher = CAR_POSITION_MESSAGE.matcher(e.getMessage().getUnformattedText());
         if (carPositionMessageMatcher.find()) {
+            executedTime = System.currentTimeMillis();
             AbstractionLayer.getPlayer().sendChatMessage("/stoproute");
             AbstractionLayer.getPlayer().sendChatMessage("/navi " + carPositionMessageMatcher.group(1) + "/" + carPositionMessageMatcher.group(2) + "/" + carPositionMessageMatcher.group(3));
         }
