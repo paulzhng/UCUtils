1431da40144121d38bb13d30bbd2f95cdc2ce230
Paul Zhang <me@paul-zhang.de>
2021-04-18 01:06:41 +0200

    fix tests

--- src/test/java/de/fuzzlemann/ucutils/base/text/TextUtilsTest.java
+++ src/test/java/de/fuzzlemann/ucutils/base/text/TextUtilsTest.java
@@ -20,20 +20,20 @@ class TextUtilsTest {
     @Test
     void testStripColor() {
         for (int i = 0; i <= 9; i++) {
-            assertEquals("Test", TextUtils.stripColor("§" + i + "Test"));
+            assertEquals("Test", TextUtils.stripColor("\u00A7" + i + "Test"));
         }
 
         for (char c = (int) 'a'; (int) c <= (int) 'f'; c++) {
-            assertEquals("Test", TextUtils.stripColor("§" + c + "Test"));
+            assertEquals("Test", TextUtils.stripColor("\u00A7" + c + "Test"));
         }
 
         for (char c = (int) 'k'; (int) c <= (int) 'o'; c++) {
-            assertEquals("Test", TextUtils.stripColor("§" + c + "Test"));
+            assertEquals("Test", TextUtils.stripColor("\u00A7" + c + "Test"));
         }
 
-        assertEquals("Test", TextUtils.stripColor("§rTest"));
+        assertEquals("Test", TextUtils.stripColor("\u00A7rTest"));
 
         assertEquals("Test", TextUtils.stripColor("Test"));
-        assertEquals("§hTest", TextUtils.stripColor("§hTest"));
+        assertEquals("\u00A7hTest", TextUtils.stripColor("\u00A7hTest"));
     }
 }
