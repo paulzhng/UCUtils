872713d43bdf09568b77c14ed93bd365af21387e
Dimiikou <leon.aissa@icloud.com>
2021-04-18 00:09:38 +0200

    Update CarFindEventHandler.java

--- src/main/java/de/fuzzlemann/ucutils/events/CarFindEventHandler.java
+++ src/main/java/de/fuzzlemann/ucutils/events/CarFindEventHandler.java
@@ -18,14 +18,24 @@ import java.util.regex.Pattern;
 @SideOnly(Side.CLIENT)
 public class CarFindEventHandler {
     private static final Pattern CAR_POSITION_MESSAGE = Pattern.compile("^\\[Car] Das Fahrzeug befindet sich bei . X: (-?\\d+) \\| Y: (-?\\d+) \\| Z: (-?\\d+)$");
+    private static final Pattern ROUTE_PATTERNS = Pattern.compile("^Du hast keine Route\\.$" +
+            "|^Du hast deine Route gelöscht\\.$");
+
 
     @SubscribeEvent
     public static void onChatReceived(ClientChatReceivedEvent e) {
         if (!UCUtilsConfig.autoNavigationForCarFind) return;
+
+        Matcher routeMatcher = ROUTE_PATTERNS.matcher(e.getMessage().getUnformattedText());
+        if (routeMatcher.find())
+            e.setCanceled(true);
         
         Matcher carPositionMessageMatcher = CAR_POSITION_MESSAGE.matcher(e.getMessage().getUnformattedText());
         if (carPositionMessageMatcher.find()) {
+            AbstractionLayer.getPlayer().sendChatMessage("/stoproute");
             AbstractionLayer.getPlayer().sendChatMessage("/navi " + carPositionMessageMatcher.group(1) + "/" + carPositionMessageMatcher.group(2) + "/" + carPositionMessageMatcher.group(3));
         }
+
+
     }
 }
