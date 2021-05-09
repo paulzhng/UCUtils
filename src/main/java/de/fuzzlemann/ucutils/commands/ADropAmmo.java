0d87d3eb5371d8bccf1993ed070685dd4d396de0
Dimiikou <leon.aissa@icloud.com>
2021-05-09 17:54:48 +0200

    Adds /adropammo

---
+++ src/main/java/de/fuzzlemann/ucutils/commands/ADropAmmo.java
@@ -0,0 +1,49 @@
+package de.fuzzlemann.ucutils.commands;
+
+import de.fuzzlemann.ucutils.base.abstraction.UPlayer;
+import de.fuzzlemann.ucutils.base.command.Command;
+import de.fuzzlemann.ucutils.base.command.TabCompletion;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Dimiikou
+ */
+public class ADropAmmo implements TabCompletion{
+
+    private final Timer timer = new Timer();
+    private int ammo;
+
+    @Command(value = "adropammo", usage = "/%label% [Waffe] [Munition]")
+    public boolean onCommand(UPlayer p, String weapon, int ammo) {
+        if (!weapon.equalsIgnoreCase("M4") && !weapon.equalsIgnoreCase("MP5") && !weapon.equalsIgnoreCase("Jagdlinte") && !weapon.equalsIgnoreCase("Pistole")) return false;
+
+        if (!(ammo > 0)) return false;
+
+        ADropAmmo instance = this;
+        instance.ammo = ammo;
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                if (instance.ammo > 100) {
+                    p.sendChatMessage("/dropammo " + weapon + " 100");
+                    instance.ammo -= 100;
+                } else {
+                    p.sendChatMessage("/dropammo " + weapon + " " + instance.ammo);
+                    cancel();
+                }
+            }
+        }, 0, TimeUnit.SECONDS.toMillis(1));
+
+        return true;
+    }
+
+    public List<String> getTabCompletions(UPlayer p, String[] args) {
+        return Arrays.asList("M4", "MP5", "Jagdflinte", "Pistole");
+    }
+
+}
