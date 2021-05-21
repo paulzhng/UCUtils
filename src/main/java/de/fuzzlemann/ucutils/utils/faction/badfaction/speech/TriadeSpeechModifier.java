ff3acfa6c7935a68dad012edf0210103246c2942
NiklasJan <LayTastisch@gmail.com>
2021-05-22 00:36:14 +0200

    Add new word to Triadespeech

---
+++ src/main/java/de/fuzzlemann/ucutils/utils/faction/badfaction/speech/TriadeSpeechModifier.java
@@ -0,0 +1,41 @@
+package de.fuzzlemann.ucutils.utils.faction.badfaction.speech;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * @author Fuzzlemann
+ */
+public class TriadeSpeechModifier implements SpeechModifier {
+
+    public static final List<Map.Entry<String, String>> REPLACE_IGNORE_CASE = Lists.newArrayList(
+
+            Maps.immutableEntry("Nia Hao", "Halllo"),
+            Maps.immutableEntry("Shi De", "Ja"),
+            Maps.immutableEntry("Bu Shi", "Nein"),
+            Maps.immutableEntry("Zai Jian", "Tschüss"),
+            Maps.immutableEntry("Xie Xie", "Danke")
+
+    );
+
+
+
+
+
+
+
+    @Override
+    public String turnIntoSpeech(String[] words) {
+        StringJoiner joiner = new StringJoiner(" ");
+
+        for (String word : words) {
+            joiner.add(word.replace('r', 'l').replace('R', 'L'));
+        }
+
+        return joiner.toString();
+    }
+}
