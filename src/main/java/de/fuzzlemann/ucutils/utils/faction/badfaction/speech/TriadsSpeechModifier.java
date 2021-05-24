f492100f45ab89009ed486d3c2087ff65c817fc0
NiklasJan <84045754+NiklasJan@users.noreply.github.com>
2021-05-24 22:00:26 +0200

    Add words to TriadsSpeech (#43)

--- src/main/java/de/fuzzlemann/ucutils/utils/faction/badfaction/speech/TriadsSpeechModifier.java
+++ src/main/java/de/fuzzlemann/ucutils/utils/faction/badfaction/speech/TriadsSpeechModifier.java
@@ -1,12 +1,25 @@
 package de.fuzzlemann.ucutils.utils.faction.badfaction.speech;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
 
 /**
  * @author Fuzzlemann
  */
 public class TriadsSpeechModifier implements SpeechModifier {
+    public static final List<Map.Entry<String, String>> REPLACE_IGNORE_CASE = Lists.newArrayList(
 
+            Maps.immutableEntry("Hallo", "Nia Hao"),
+            Maps.immutableEntry("Ja", "Shi De"),
+            Maps.immutableEntry("Nein", "Bu Shi"),
+            Maps.immutableEntry("Tschüss", "Zai Jian"),
+            Maps.immutableEntry("Danke", "Xie Xie")
+    );
+    
     @Override
     public String turnIntoSpeech(String[] words) {
         StringJoiner joiner = new StringJoiner(" ");
