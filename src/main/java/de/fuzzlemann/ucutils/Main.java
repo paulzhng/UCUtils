9e807c065f7e43cfb95967aab584fb7ad7d6031a
lholl <leo.holl2001@gmail.com>
2022-05-23 11:48:14 +0200

    Update to version 1.10

--- src/main/java/de/fuzzlemann/ucutils/Main.java
+++ src/main/java/de/fuzzlemann/ucutils/Main.java
@@ -30,7 +30,7 @@ public class Main {
     public static final Minecraft MINECRAFT = Minecraft.getMinecraft();
 
     public static final String MOD_ID = "ucutils";
-    public static final String VERSION = "1.12.2-1.9";
+    public static final String VERSION = "1.12.2-1.10";
 
     static final String NAME = "UC Utils";
 
